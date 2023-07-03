46131ae9672d97e74602843333fb4aa99a9a546f
calumtreloar <calumtreloar@gmail.com>
2023-07-04 04:36:26 +0930

    Add calculator buttons and number display

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ let firstNum = 0;
 let operator = "";
 let lastNum = 0;
 
+const currentDisplay = document.querySelector(".current");
+const numberButtons = document.querySelectorAll(".number").forEach((el) => {
+  el.addEventListener("click", () => {
+    console.log("The numbers work!");
+    currentDisplay.innerText += el.innerText;
+  });
+});
+
+numberButtons.addEventListener("click", () => {
+  console.log("The numbers work");
+});
+
 const add = (...args) => {
   return args.reduce((total, current) => total + current, 0);
 };
