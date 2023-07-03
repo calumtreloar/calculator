b61f0166b089ee45cc73f9560c77340b164fdfde
calumtreloar <calumtreloar@gmail.com>
2023-07-03 20:16:01 +0930

    create basic calculator layout

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum = 0;
+let operator = "";
+let lastNum = 0;
+
 const add = (...args) => {
   return args.reduce((total, current) => total + current, 0);
 };
@@ -14,6 +18,6 @@ const divide = (...args) => {
   return args.reduce((total, current) => total / current);
 };
 
-let firstNum = 0;
-let operator = "";
-let lastNum = 0;
+function operate(operator, num1, num2) {
+  add(num1, num2);
+}
